85ca68642e439201138556bb014c27bbf082e30e
criss kimaryo <criss kimaryo>
2016-11-02 01:44:34 +0300

    edit and update some feature

--- src/pages/book/toread/toread.ts
+++ src/pages/book/toread/toread.ts
@@ -6,12 +6,46 @@ import { ReadPage } from '../read/read';
   templateUrl: 'toread.html'
 })
 export class ToreadPage {
-
+  storieslist: any = [
+    {
+      title: "mama ni mama",
+      chapter: "sehemu ya 1",
+    },
+    {
+      title: "utamu wa pesa",
+      chapter: "sehemu ya 2",
+    },
+    {
+      title: "sitaki nataka",
+      chapter: "sehemu ya 3",
+    },
+    {
+      title: "nipe yeto",
+      chapter: "sehemu ya 4",
+    },
+    {
+      title: "sikio lakufa",
+      chapter: "sehemu ya 5",
+    },
+    {
+      title: "nipe nikupe",
+      chapter: "sehemu ya 5",
+    },
+    {
+      title: "pale kati ....",
+      chapter: "sehemu ya 5",
+    },
+  ];
+  choosetab: any;
   constructor(public navCtrl: NavController) {
     console.log("hey toread")
+    this.choosetab = "book"
   }
 
   read() {
     this.navCtrl.push(ReadPage);
+  }
+   back() {
+    this.navCtrl.pop();
   }
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
-
 import { NavController } from 'ionic-angular';
-
 import { ToreadPage } from '../book/toread/toread';
 
 @Component({
@@ -11,26 +9,77 @@ import { ToreadPage } from '../book/toread/toread';
 export class HomePage {
   // for testing array data in html
   jj: any = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-  images: any = [
+  images: any[];
 
-     {
-      img:"img/katuni/masoudi.jpeg",
-    },
-     {
-      img:"img/katuni/love tz.jpeg",
-    },
-     {
-      img:"img/katuni/ibra.jpeg",
-    },
-     {
-      img:"img/katuni/simba.jpeg",
-    },
-  ];
+
+  searchQuery: string = '';
+
 
   constructor(public navCtrl: NavController) {
+    this.initializedata();
   }
 
- navstry() {
+  initializedata() {
+    this.images = [
+
+       {
+        title: "masudi kipanya",
+        img: "img/katuni/mombasa kisiwa.jpeg",
+      },
+      {
+        title: "masai mjini",
+        img: "img/katuni/masai.jpeg",
+      },
+      {
+        title: "traffic noma",
+        img: "img/katuni/traffic.jpeg",
+      },
+      {
+        title: "simba mtaita",
+        img: "img/katuni/simba.jpeg",
+      },
+      {
+        title: "masudi kipanya",
+        img: "img/katuni/masoudi.jpeg",
+      },
+      {
+        title: "kibonzo na jiji",
+        img: "img/katuni/vibonjo.jpeg",
+      },
+        {
+        title: "love for tz",
+        img: "img/katuni/love tz.jpeg",
+      },
+         {
+        title: "madenge",
+        img: "img/katuni/madenge.jpeg",
+      },
+      {
+        title: "maadili social",
+        img: "img/katuni/maadili socialm.jpeg",
+      },
+        {
+        title: "mafuriko",
+        img: "img/katuni/mafuriko.jpeg",
+      },
+    ];
+  }
+
+  getItems(ev: any) {
+    // Reset items back to all of the items
+    this.initializedata();
+
+    // set val to the value of the searchbar
+    let val = ev.target.value;
+
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      this.images = this.images.filter((item) => {
+        return ( item.title.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
+  }
+  navstry() {
 
     this.navCtrl.push(ToreadPage)
   }
