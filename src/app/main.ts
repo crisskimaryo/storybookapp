d3e900203e8305be7f944efb0dd5e8eac09fd93e
NAMANGA <NAMANGA>
2016-12-26 14:58:19 +0300

    update new realise dependance

---
+++ src/app/main.ts
@@ -0,0 +1,7 @@
+// import { platformBrowser } from '@angular/platform-browser';
+// import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app.module';
+
+platformBrowserDynamic().bootstrapModule(AppModule);
